f1d7c8c92dd0e1eca3cbbbebae348b39118ed2aa
sbsteacher <id@gmail.com>
2025-05-14 18:07:04 +0900

    Day.05

---
+++ src/ch04/Mission06_01.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class Mission06_01 {
+    public static void main(String[] args) {
+        //1~100까지 숫자 출력
+        //int i=5; 수정 불가능. 나머지는 수정 가능
+        //결과는 같게 해주세요.
+        for(int i=5; i<=100; i++) {
+            System.out.println(i);
+        }
+        //1
+        //2
+        //3
+        //4
+        //5 ...
+        //100
+    }
+}
---
+++ src/ch04/Mission13.java
@@ -0,0 +1,23 @@
+package ch04;
+
+public class Mission13 {
+    public static void main(String[] args) {
+        //int star = (int)(Math.random() * 6) + 3; //3~8까지 랜덤값
+
+        //System.out.println(   Math.random() * 6   );
+        System.out.println(  (int)(Math.random() * 6)   ); // 0~5
+        System.out.println(  (int)(Math.random() * 6) + 3  ); // 3~8
+
+        //star = 3
+        //*
+        //**
+        //***
+
+        //star = 5
+        //*
+        //**
+        //***
+        //****
+        //*****
+    }
+}
